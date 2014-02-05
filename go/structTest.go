841a4e27e1e682746c46eba1650a4e6b2cfae951
Robert Skelton <robertjskelton@gmail.com>
2014-02-04 20:10:36 -0600

    update go's

--- go/structTest.go
+++ go/structTest.go
@@ -2,9 +2,9 @@
 *
 * Author: 		Robert Skelton
 * Date 			2.4.14
-* Filename: 	hello.go
+* Filename: 	structTest.go
 * Email: 		robertjskelton@gmail.com
-* Description:  Basic go testing.
+* Description:  Basic structs in go testing.
 *
 */
 
@@ -16,12 +16,11 @@ type Rectangle struct {
 	width, height int
 }
 
-func (r *Rectangle) area() int {
-	//return float64(r.height * r.width)
-	return r.height * r.width
+func (r *Rectangle) area() float64 {
+	return float64(r.height * r.width)
 }
 
 func main() { 
-	r := Rectangle{6,8}
-	fmt.Println(r.area)
+	r := Rectangle{5, 12}
+	fmt.Println(r.area())
 }
