a479751aabdf1c11d4ed0d2688db436ae36b96b1
Robert Skelton <robertjskelton@gmail.com>
2016-12-15 23:52:01 -0500

    beer

---
+++ C++/coutstout.cpp
@@ -0,0 +1,15 @@
+/*
+*  Robert Skelton
+*  Bottled: 9.8.2015
+*  Oaked imperial stout 
+*  with vanilla
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Stout\n";
+	return 0;
+}
+
